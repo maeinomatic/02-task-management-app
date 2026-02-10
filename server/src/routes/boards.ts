7211d085b20ae422f685e6efbe07fa5b129f7b93
Your Name <your-email@example.com>
2026-02-10 17:04:55 +0100

    feat: add OpenAPI documentation for boards and cards routes

--- server/src/routes/boards.ts
+++ server/src/routes/boards.ts
@@ -1,3 +1,90 @@
+/**
+ * @openapi
+ * /api/boards:
+ *   get:
+ *     summary: Get all boards
+ *     tags:
+ *       - Boards
+ *     responses:
+ *       200:
+ *         description: List of boards
+ *   post:
+ *     summary: Create a new board
+ *     tags:
+ *       - Boards
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               title:
+ *                 type: string
+ *               description:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Board created
+ *
+ * /api/boards/{id}:
+ *   get:
+ *     summary: Get a board by ID
+ *     tags:
+ *       - Boards
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Board found
+ *       404:
+ *         description: Board not found
+ *   put:
+ *     summary: Update a board
+ *     tags:
+ *       - Boards
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               title:
+ *                 type: string
+ *               description:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Board updated
+ *       404:
+ *         description: Board not found
+ *   delete:
+ *     summary: Delete a board
+ *     tags:
+ *       - Boards
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Board deleted
+ *       404:
+ *         description: Board not found
+ */
 // src/routes/boards.ts
 import express from 'express';
 import { getBoards, getBoard, createBoard, updateBoard, deleteBoard } from '../controllers/boardController.js';
--- server/src/routes/cards.ts
+++ server/src/routes/cards.ts
@@ -1,3 +1,94 @@
+/**
+ * @openapi
+ * /api/cards:
+ *   get:
+ *     summary: Get all cards
+ *     tags:
+ *       - Cards
+ *     responses:
+ *       200:
+ *         description: List of cards
+ *   post:
+ *     summary: Create a new card
+ *     tags:
+ *       - Cards
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               title:
+ *                 type: string
+ *               description:
+ *                 type: string
+ *               listId:
+ *                 type: integer
+ *     responses:
+ *       201:
+ *         description: Card created
+ *
+ * /api/cards/{id}:
+ *   get:
+ *     summary: Get a card by ID
+ *     tags:
+ *       - Cards
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Card found
+ *       404:
+ *         description: Card not found
+ *   put:
+ *     summary: Update a card
+ *     tags:
+ *       - Cards
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               title:
+ *                 type: string
+ *               description:
+ *                 type: string
+ *               listId:
+ *                 type: integer
+ *     responses:
+ *       200:
+ *         description: Card updated
+ *       404:
+ *         description: Card not found
+ *   delete:
+ *     summary: Delete a card
+ *     tags:
+ *       - Cards
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Card deleted
+ *       404:
+ *         description: Card not found
+ */
 // src/routes/cards.ts
 import express from 'express';
 import { getCards, getCard, createCard, updateCard, deleteCard } from '../controllers/cardController.js';
